4f7b7b5e5d503ae300b58354083440fc8ac202ec
Abhishek Airan <cubicfammy@gmail.com>
2025-06-05 09:58:37 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  basePath: '/Taxi_managment', // Replace with your repository name
   allowedDevOrigins: ['*'],
 };
 
